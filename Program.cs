1a27d6e9ddd32c0d90853604341a2f1eb3a2163a
RahulAid <raccess97@gmail.com>
2022-10-22 17:11:42 +0530

    Program to Calculate Quotient and Remainder

--- Program.cs
+++ Program.cs
@@ -5,19 +5,16 @@
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Enter a four digit number");
-            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Dividend value : ");
+            int Dividend = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Divisor value : ");
+            int Divisor = Convert.ToInt32(Console.ReadLine());
 
-            if (((num % 4 == 0) && (num % 100 != 0)) || (num % 400 == 0))
-            {
-                Console.WriteLine(+num + " is a Leap Year ");
+            int Quotient = Dividend / Divisor;
+            int Remainder = Dividend % Divisor;
 
-            }
-            else
-            {
-                Console.WriteLine(+num + " is not a Leap Year");
-            }
-            
+            Console.WriteLine("The Quotient is : " + Quotient);
+            Console.WriteLine("The Remainder is : " + Remainder);
             Console.ReadLine();
 
         }
