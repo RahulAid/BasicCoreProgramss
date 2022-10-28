d71af7e03c7f76a4d59a460bdc7d0527d56427b7
RahulAid <raccess97@gmail.com>
2022-10-28 23:19:51 +0530

    Program to Print the Numbers which are Powers of Two

--- Program.cs
+++ Program.cs
@@ -5,29 +5,18 @@
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Enter an Alphabet from a to z : ");
-            char n = Convert.ToChar(Console.ReadLine());
-
-            switch (n)
+            Console.WriteLine("Enter a Number whose Powers are to be calculated : ");
+            long N = Convert.ToInt32(Console.ReadLine());
+            int num = 1;
+            Console.WriteLine("The powers of " + N + " are");
+            for (int i = 2; i < N; i++)
             {
-                case 'a':
-                    Console.WriteLine(n+" is a Vowel");
-                    break;
-                case 'e':
-                    Console.WriteLine(n+" is a Vowel");
-                    break;
-                case 'i':
-                    Console.WriteLine(n+" is a Vowel");
-                    break;
-                case 'o':
-                    Console.WriteLine(n+" is a Vowel");
-                    break;
-                case 'u':
-                    Console.WriteLine(n+" is a Vowel");
-                    break;
-                default:
-                    Console.WriteLine(n+" is a Consonant");
-                    break;
+                 num = num * 2;
+                if ( num < N)
+                {
+                    Console.WriteLine(num);
+                }
+                
             }
             Console.ReadLine();
 
