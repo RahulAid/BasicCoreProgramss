bdc8b2bd272ca2ab6d84e2c3faf9dd7672b15329
RahulAid <raccess97@gmail.com>
2022-10-31 00:28:17 +0530

    Harmonic Number Series

--- Program.cs
+++ Program.cs
@@ -2,27 +2,20 @@
 {
     internal class Program
     {
-        
+
         static void Main(String[] args)
         {
-            Console.WriteLine("Enter a value: ");
-            int Num = Convert.ToInt32(Console.ReadLine());
-            int i;
-            
-            for (i = 2; i <= Num; i++)
+            float count;
+            float sum = 0;
+
+            Console.WriteLine("Enter Nth Value : ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            for (count = 1; count <= N; count++)
             {
-                if (Num % i == 0)
-                {
-                    Console.WriteLine(i+" is a Factor of " + Num);
-
-                }
-                else
-                {
-                    Console.WriteLine(i + " is not a Factor of " + Num);
-                }
+                 sum += 1 / count;
             }
+            Console.WriteLine("Sum of Harmonic numbers series is : {0}",sum);
             Console.ReadLine();
-
         }
     }
 }
